f937a30991ef0f0978cdd6f2f1f69127e7e781a1
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-23 13:40:06 +0700

    debug

--- web/auth.ts
+++ web/auth.ts
@@ -10,14 +10,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         console.log("Profile Google Eksekusi");
         const { email } = profile;
 
-        const { data } = await userTable
-          .select("*")
-          .eq("email", email)
-          .maybeSingle();
+        const { data } = await userTable.select("*").eq("email", email);
+
+        if (!data) throw new Error("Data user tidak ada");
 
         console.log("Data user berhasil diambil");
         console.log(data);
-        const user = mapDbUserToClient(data);
+        const user = mapDbUserToClient(data[0]);
         console.log("Data user berhasil diconvert");
         console.log(user);
 
@@ -39,13 +38,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       console.log("Cek data selesai");
 
       if (!userDb || userError) {
-        console.log("terjadi error")
-        console.log(userError)
-        console.log(userDb)
+        console.log("terjadi error");
+        console.log(userError);
+        console.log(userDb);
         return false;
       }
 
-      console.log("user berhasil login")
+      console.log("user berhasil login");
       return true;
     },
     jwt(params) {
