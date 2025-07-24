c9834cc3cc7fa232880218191007dc205436dbab
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-24 10:11:21 +0700

    Debugging

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -1,8 +1,10 @@
 import UserProfileTemplate from "@/components/templates/ProfileTemplate";
 import { getAllTask } from "@/utils/supabase/taskTable";
 
+export const dynamic = "force-dynamic"
+
 export default async function ProfilePage() {
   const tasks = await getAllTask();
-  console.log(`Tasks from Server`, tasks);
+
   return <UserProfileTemplate tasks={tasks} />;
 }
