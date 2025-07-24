b7f9194d5f4b7192e1d3202607b4f3539d4b3eeb
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-24 09:45:30 +0700

    Profile tasks

--- web/app/profile/page.tsx
+++ web/app/profile/page.tsx
@@ -1,29 +1,7 @@
-import { Task } from "@/@types/tasks";
 import UserProfileTemplate from "@/components/templates/ProfileTemplate";
-import { mapDbTaskToClient } from "@/lib/map-data/mapDbTaskToClient";
-import { tasksTable } from "@/utils/supabase/client";
-
-const getTasks = async () => {
-  const { data, error } = await tasksTable.select("*");
-
-  if (error) {
-    console.log(error);
-    throw error;
-  }
-
-  if(!data){
-    return []
-  }
-  const tasks:Task[]= [];
-
-  for(const d of data){
-    tasks.push(mapDbTaskToClient(d));
-  }
-
-  return tasks
-};
+import { getAllTask } from "@/utils/supabase/taskTable";
 
 export default async function ProfilePage() {
-  const tasks = await getTasks()
+  const tasks = await getAllTask();
   return <UserProfileTemplate initialTasks={tasks} />;
 }
