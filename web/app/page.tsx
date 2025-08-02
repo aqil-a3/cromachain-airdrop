4be6dbd6d0778942cf90ab21159ef988b9df4adf
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-08-02 11:41:04 +0700

    Force dynamic

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,8 @@
 import HomeTemplate from "@/components/templates/HomeTemplate";
 import { getActiveAirdrop } from "@/utils/supabase/airdropTable";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const airdrop = await getActiveAirdrop();
 
