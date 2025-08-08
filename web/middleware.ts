379fd3dbfd1ab3e4fc80c289432ff0f2c93fcfec
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-08-08 14:44:53 +0700

    MT start

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,26 +1,38 @@
 // export { auth as middleware } from "@/auth"
 
+import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+// Konfigurasi
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 const IS_MAINTENANCE = true;
 
-export function middleware(request: NextRequest) {
+// Middleware utama
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isApiRoute = pathname.startsWith("/api");
   const isMaintenancePage = pathname.startsWith("/maintenance");
 
-  if (IS_PRODUCTION && IS_MAINTENANCE && !isApiRoute && !isMaintenancePage) {
+  // Bypass semua logic kalau path /maintenance
+  if (isMaintenancePage) {
+    return NextResponse.next();
+  }
+
+  // Redirect ke maintenance page jika sedang maintenance
+  if (IS_PRODUCTION && IS_MAINTENANCE && !isApiRoute) {
     const maintenanceUrl = request.nextUrl.clone();
     maintenanceUrl.pathname = "/maintenance";
     return NextResponse.redirect(maintenanceUrl);
   }
 
-  return NextResponse.next();
+  // Lanjutkan ke middleware auth (biar session tetap hidup)
+  //   @ts-ignore
+  return auth(request);
 }
 
+// Middleware hanya aktif untuk path-path tertentu
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|api).*)"],
+  matcher: ["/((?!_next|favicon.ico).*)"],
 };
