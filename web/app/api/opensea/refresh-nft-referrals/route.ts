0898b8ff29e61ca34bb965442f8cc9da798f0076
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-08-30 10:45:22 +0700

    Update Cron Test

--- web/app/api/opensea/refresh-nft-referrals/route.ts
+++ web/app/api/opensea/refresh-nft-referrals/route.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const headers = req.headers;
-  const secretKey = headers.get("Authorization");
-  const envKey = `${process.env.OPENSEA_CRON_SECRET_KEY}`;
-  console.log("Running")
+  const authHeader = req.headers.get("Authorization") || "";
+  const secretKey = authHeader.replace("Bearer ", "");
+  const envKey = process.env.OPENSEA_CRON_SECRET_KEY;
+  console.log("Running");
 
   if (!secretKey || secretKey !== envKey)
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
 
-
-  return NextResponse.json({message:"OK"});
+  return NextResponse.json({ message: "OK" });
 }
+    
