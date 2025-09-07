70d8e1d36a929264e4e8b31a2280e1d8e831bbf4
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-09-07 09:06:27 +0700

    Update API

--- web/app/api/opensea/verify-task/route.ts
+++ web/app/api/opensea/verify-task/route.ts
@@ -1,4 +1,5 @@
-import { TaskUser } from "@/@types/task-user";
+import { BasicHttpResponse } from "@/@types/http";
+import { TaskUser, TaskUserDb } from "@/@types/task-user";
 import { auth } from "@/auth";
 import { opensea } from "@/utils/opensea/api";
 import { gettaskRewardByTaskIdBulks } from "@/utils/supabase/taskTable";
@@ -13,14 +14,18 @@ const CROMA_SMART_CONTRACT = "0x1d53b6c882ac7fbd5ea0eeca00d59b843ef50525";
 
 // const BASE_URL = "http://localhost:3000";
 const BASE_URL = "https://airdrop.cromachain.com";
-const allowedToken = ["8", "10"];
+const allowedToken = ["8", "10", "9", "12"];
 
+// Verif Task Id in first
+// Second is buy task
 const taskIds: Record<string, string[]> = {
   "8": [
-    "f7f89788-d176-4d8c-a5e4-f87f4500bb1e",
     "06e075ae-721f-416a-9251-07db24c05c40",
+    "f7f89788-d176-4d8c-a5e4-f87f4500bb1e",
   ],
+  "9": ["4038bc6d-a2ce-4bb1-8ced-9e91468197b1"],
   "10": ["11f4a316-5a69-4893-b70e-79e260c2aca3"],
+  "12": ["ee74e98e-eb6b-4dbf-afd6-a02550489573"],
 };
 
 export async function GET(req: NextRequest) {
@@ -77,82 +82,12 @@ export async function GET(req: NextRequest) {
       )
     );
 
-  //   Pertama-tama, ambil data dari tugas terkait
-  if (token === "8") {
-    const TASK_IDS: string[] = taskIds["8"];
-    const tasks = await gettaskRewardByTaskIdBulks(TASK_IDS);
-    const verifTask = await getUserTaskByTaskIdAndUserId(
-      "06e075ae-721f-416a-9251-07db24c05c40",
-      userId!
+  const { success, message } = await verifyTask(taskIds[token], userId!);
+
+  if (!success)
+    return NextResponse.redirect(
+      new URL(`/profile?opensea-task-message=${message}`, BASE_URL)
     );
-    const buyTask = await getUserTaskByTaskIdAndUserId(
-      "f7f89788-d176-4d8c-a5e4-f87f4500bb1e",
-      userId!
-    );
-
-    const combinedTask = [...verifTask, ...buyTask];
-
-    if (verifTask[0].status === "completed")
-      return NextResponse.redirect(
-        new URL(
-          "/profile?opensea-task-message=You've finished the task.",
-          BASE_URL
-        )
-      );
-
-    //   Selanjutnya, mapping agar sesuai bentuk DB
-    const payloads: TaskUser[] = tasks.map((task) => {
-      const selectedTaskUser = combinedTask.find(
-        (tur) => tur.task_id === task.id
-      );
-
-      return {
-        id: selectedTaskUser?.id,
-        taskId: task.id,
-        userId: userId!,
-        status: "completed",
-        rewardEarned: task.reward,
-        rewardType: task.reward_type,
-      };
-    });
-
-    //   Simpan
-    await updateStatusUserTasksBulks(payloads, "completed");
-  } else if (token === "10") {
-    const TASK_IDS: string[] = ["11f4a316-5a69-4893-b70e-79e260c2aca3"];
-    const tasks = await gettaskRewardByTaskIdBulks(TASK_IDS);
-    const verifTask = await getUserTaskByTaskIdAndUserId("11f4a316-5a69-4893-b70e-79e260c2aca3", userId!);
-
-    const combinedTask = [...verifTask];
-
-    if (verifTask[0].status === "completed")
-      return NextResponse.redirect(
-        new URL(
-          "/profile?opensea-task-message=You've finished the task.",
-          BASE_URL
-        )
-      );
-
-    //   Selanjutnya, mapping agar sesuai bentuk DB
-    const payloads: TaskUser[] = tasks.map((task) => {
-      const selectedTaskUser = combinedTask.find(
-        (tur) => tur.task_id === task.id
-      );
-
-      return {
-        id: selectedTaskUser?.id,
-        taskId: task.id,
-        userId: userId!,
-        status: "completed",
-        rewardEarned: task.reward,
-        rewardType: task.reward_type,
-      };
-    });
-
-    //   Simpan
-    await updateStatusUserTasksBulks(payloads, "completed");
-    return NextResponse.json({ message: "Tinggal nunggu taskID" });
-  }
 
   return NextResponse.redirect(
     new URL(
@@ -161,3 +96,52 @@ export async function GET(req: NextRequest) {
     )
   );
 }
+
+const verifyTask = async (
+  TASK_IDS: string[],
+  userId: string
+): Promise<BasicHttpResponse> => {
+  const [verifTaskId, buyTaskId] = TASK_IDS;
+  const tasks = await gettaskRewardByTaskIdBulks(TASK_IDS);
+  const verifTask = await getUserTaskByTaskIdAndUserId(verifTaskId, userId);
+  if (!verifTask || verifTask.length === 0)
+    return {
+      success: false,
+      message:
+        "We can't find your progress task in our system. Have you started a task?",
+    };
+
+  let buyTask: TaskUserDb[] = [];
+  if (buyTaskId) {
+    buyTask = await getUserTaskByTaskIdAndUserId(buyTaskId, userId!);
+  }
+  const combinedTask = [...verifTask, ...buyTask];
+
+  if (verifTask[0].status === "completed")
+    return {
+      success: false,
+      message: "You've finished the task.",
+    };
+  //   Selanjutnya, mapping agar sesuai bentuk DB
+  const payloads: TaskUser[] = tasks.map((task) => {
+    const selectedTaskUser = combinedTask.find(
+      (tur) => tur.task_id === task.id
+    );
+
+    return {
+      id: selectedTaskUser?.id,
+      taskId: task.id,
+      userId: userId!,
+      status: "completed",
+      rewardEarned: task.reward,
+      rewardType: task.reward_type,
+    };
+  });
+
+  //   Simpan
+  await updateStatusUserTasksBulks(payloads, "completed");
+  return {
+    success: true,
+    message: "Success",
+  };
+};
